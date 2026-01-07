703b1f2084b50e9da0fc078a4be9a0c81bab0b13
Harshad <jalondharaharshad007@gmail.com>
2026-01-07 13:32:21 +0530

    full project

---
+++ src/components/Cart.jsx
@@ -0,0 +1,65 @@
+import { useCart } from "../context/CartContext"
+
+
+const Cart = () => {
+    const { cart, removeProduct, updateQuantity, total } = useCart();
+
+
+
+    return (
+
+        <div className="container">
+            <h2 className="mb-3">Your Cart</h2>
+
+            {/* condition */}
+
+            {cart.length === 0 ? (
+                <div className="alert alert-info">No Item In Cart</div>
+            ) : (
+                <>
+                <table className="table table-bordered align-middle">
+                    <thead className="table-light">
+                        <tr>
+                            <th>Product</th>
+                            <th>Price</th>
+                            <th>Qty</th>
+                            <th>Subtotal</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item) => (
+                                <tr key={item.id}>
+                                    <td>{item.name}</td>
+                                    <td>${item.price}</td>
+                                    <td style={{width: "100px"}}>
+                                        <input
+                                            className="form-control text-center"
+                                            min="1"
+                                            type="number"
+                                            value={item.qty}
+                                            onChange={(e) => updateQuantity(item.id, Number(e.target.value))}
+                                        />
+                                    </td>
+                                    <td>${item.price * item.qty}</td>
+                                    <td>
+                                        <button
+                                        onClick={() => removeProduct(item.id)}
+                                        className="btn btn-outline-danger btn-sm">Remove</button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+                <div className="text-end fw-bold fs-5">Total: ${total.toFixed(2)}</div>
+                </>
+            )}
+            
+        </div>
+
+    )
+}
+
+export default Cart
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,46 +1,79 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const CartContext = createContext();
 
-const CartProvider = ({children}) => {
-    const [cart, setCart] = useState([]);
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState(() => {
+    const saved = localStorage.getItem("cart"); // data lave
+    return saved ? JSON.parse(saved) : []; // string to => arrya and object
+  });
 
-    // Add product
-    const addProduct = (product) => { // product => new value
-        setCart((prev) => { // setCart => old value
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart)); //save
+  }, [cart])
 
-            // Existing Product
-            const existingProduct = prev.find((item) => item.id === product.id);
+  // Add Product
+  const addProduct = (product) => {
+    // product => new value
+    setCart((prev) => {
+      // setCart => old value
 
-            // condition
-            if(existingProduct){
-                // const product = [{id:1, name:"laptop", price:800, qty:1}];
-                // const a = {id:1}
-                // const updatedProduct =product.map((item) => {
-                //     return item.id === a.id ? {...item, qty: item.qty + 1} : item 
-                // })
-                // console.log(updatedProduct);
+      // Existing Product
+      const existingProduct = prev.find((item) => item.id === product.id);
 
-                return prev.map((item) => item.id === product.id ? {...item, qty: item.qty + 1} : item);
+      // condition
+      if (existingProduct) {
+        // const product = [{id:1, name:"laptop", price:800, qty:1}];
+        // const a = {id:1}
+        // const updatedProduct =product.map((item) => {
+        //     return item.id === a.id ? {...item, qty: item.qty + 1} : item
+        // })
+        // console.log(updatedProduct);
 
-            }
+        return prev.map((item) =>
+          item.id === product.id ? { ...item, qty: item.qty + 1 } : item
+        );
+      }
 
-            // Not Existing Product
-            return [...prev, {...product, qty: 1} ]
+      // Not Existing Product
+      return [...prev, { ...product, qty: 1 }];
+    });
+  };
 
+  // Remove Product
+  const removeProduct = (id) => {
+    setCart((prev) => {
+       return prev.filter((item) => { 
+         return   item.id !== id       // value (false) => item remove
+        })
+    })
+  }
+
+  // Update Quantity
+    const updateQuantity = (id, qty) => {
+        setCart((prev) => {
+          return  prev.map((item) => (
+                item.id === id ? {...item, qty} : item // { qty } value replay thay se 
+            ))
         })
     }
 
-    return (
-        <CartContext.Provider value={{cart, setCart, addProduct}}> 
-            {children} {/* bas children j hal se */}
-        </CartContext.Provider>
-    )
-    
-}
+  // Total 
+
+  const total = cart.reduce((sum, item) => sum + (item.price * item.qty), 0);
+
+  return (
+    <CartContext.Provider
+      value={{ cart, addProduct, removeProduct, updateQuantity, total }}
+    >
+      {" "}
+      {/* Data bhejva mate CartProvider ni under batha tag nakha*/}
+      {children} {/* bas children j hal se */}
+    </CartContext.Provider>
+  );
+};
 
 // Custom Hook
 const useCart = () => useContext(CartContext);
 
-
-export {CartProvider, useCart} 
+export { CartProvider, useCart };
